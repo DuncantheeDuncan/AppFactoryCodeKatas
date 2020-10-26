962bcb67a3b6fb86e5b684240bcb2a0111fac0b1
DuncantheeDuncan <mthembup69@gmail.com>
2020-10-26 12:07:20 +0200

    pulling

--- src/main/java/gameOfLife/Grid.java
+++ src/main/java/gameOfLife/Grid.java
@@ -2,11 +2,13 @@ package gameOfLife;
 
 class Grid {
 
+    public  static final String TEXT_RESET = "\u001B[0m";
     public static void main(String[] args) {
 
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("enter a number...");
 //        int j = scanner.nextInt();
+        System.out.println("co\n"+ TEXT_RESET);
 
         Board printBoard = new Board();
 
