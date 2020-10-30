d234894feca35765120deae47f245162bc31f0a5
DuncantheeDuncan <mthembup69@gmail.com>
2020-10-30 12:22:29 +0200

    updated test.sh file

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
-mvn -Dtest=BoardEmptyGridTest,BoardInitializePresetTest test
+mvn -Dtest=BoardEmptyGridTest test
+mvn -Dtest=BoardInitializePresetTest test
 mvn -Dtest=BoardTest test
 mvn -Dtest=BoardThirdGenTest test
 
